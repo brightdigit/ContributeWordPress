0ddc1365f22e826af85c9c9672a36dedd95d7146
Mikaela Caron <10061622-mikaelacaron@users.noreply.gitlab.com>
2021-12-04 17:29:47 +0000

    Create Services Section

---
+++ Sources/BrightDigitKit/Publish/Strings.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+enum Strings {
+  enum Services {
+    static let heroOption1 = "We are the choice of agencies and companies, large and small, that need specialist Swift development support or want Swift-based apps built from the ground up."
+
+    static let iOSDevelopment = "BrightDigit can help you get your app into the hands of your customers, and open new markets for your business."
+
+    static let watchOSDevelopment = "Put the power of your app on Apple’s smallest screen. We have what you need to delight your on-the-go users."
+
+    static let appleDevelopment = "BrightDigit can meet all your needs to deploy your app for any Apple device. Leo puts his expertise in Swift-based development to work for you."
+
+    static let appDevelopment = ["If you need to delight your customers on phone, tablet, desktop, or watch, we have the know-how to make that happen.",
+
+                                 "We offer specialist support in developing any kind of app built using Swift."]
+    static let consulting = "If you’re still in the idea & design stage or looking to grow or change your app, we can assist you in finding opportunities and resources for deciding your direction."
+  }
+}
