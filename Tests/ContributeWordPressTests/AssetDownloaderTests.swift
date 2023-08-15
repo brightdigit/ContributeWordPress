efd6af3b79f0a4cb89c7a2f44b25c97b4f956473
Leo Dion <leogdion@brightdigit.com>
2023-08-15 11:32:01 -0400

    next unit tests

---
+++ Tests/ContributeWordPressTests/AssetDownloaderTests.swift
@@ -0,0 +1,16 @@
+//
+//  AssetDownloaderTests.swift
+//  
+//
+//  Created by Leo Dion on 8/15/23.
+//
+
+import XCTest
+
+final class AssetDownloaderTests: XCTestCase {
+
+    func testDownload() throws {
+      XCTFail("Not implemented")
+
+    }
+}
---
+++ Tests/ContributeWordPressTests/AssetImportTests.swift
@@ -0,0 +1,17 @@
+//
+//  AssetImportTests.swift
+//  
+//
+//  Created by Leo Dion on 8/15/23.
+//
+
+import XCTest
+
+final class AssetImportTests: XCTestCase {
+
+
+    func testExtractAssetImports() throws {
+      XCTFail("Not implemented")
+    }
+
+}
