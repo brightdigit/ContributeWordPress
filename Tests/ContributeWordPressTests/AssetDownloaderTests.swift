58b5050ea9d160fc3aa2ff556ab7759058d307b8
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2023-08-15 22:27:41 +0300

    test: AssetDownloader

--- Tests/ContributeWordPressTests/AssetDownloaderTests.swift
+++ Tests/ContributeWordPressTests/AssetDownloaderTests.swift
@@ -1,12 +1,61 @@
-//
-//  AssetDownloaderTests.swift
-//  
-//
-//  Created by Leo Dion on 8/15/23.
-//
-
 import XCTest
+import Contribute
+@testable import ContributeWordPress
 
-final class AssetDownloaderTests: XCTestCase {
+internal final class AssetDownloaderTests: XCTestCase {
+  func testSuccessDownload() throws {
+    let downloader = AssetDownloader(urlDownloader: MockSuccessFileDownloader())
 
+    let assets: [AssetImport] = try myYearInReviewAssets()
+
+    XCTAssertNoThrow(
+      try downloader.download(assets: assets,allowsOverwrites: false)
+    )
+  }
+
+  func testFailureDownload() throws {
+    let downloader = AssetDownloader()
+
+    let assets: [AssetImport] = try myYearInReviewAssets()
+
+    XCTAssertThrowsError(
+      try downloader.download(assets: assets, allowsOverwrites: false)
+    ) { throwError in
+      guard case WordPressError.assetDownloadErrors(let errors) = throwError else {
+        XCTFail("Expected associated list of url->error.")
+        return
+      }
+
+      let failedURLs: [URL] = Array(errors.keys)
+
+      for index in failedURLs.indices {
+        let asset = assets[index]
+        let url = failedURLs[index]
+
+        XCTAssertEqual(url, asset.fromURL)
+      }
+    }
+  }
+
+  private func myYearInReviewAssets() throws -> [AssetImport] {
+    [
+      AssetImport(
+        forPost: try .myYearInReviewPost(),
+        sourceURL: .temporaryResourcesPathURL,
+        assetRoot: "/media/wp-assets",
+        resourcesPathURL:  URL(string: "https://leogdion.name/wp-content/uploads/2019/06/aleks-dorohovich-26-unsplash-701.jpeg")!,
+        importPathURL: nil
+      )
+    ]
+  }
+}
+
+private final class MockSuccessFileDownloader: URLDownloader {
+  func download(
+    from fromURL: URL,
+    to toURL: URL,
+    allowOverwrite: Bool,
+    _ completion: @escaping (Error?) -> Void) {
+      completion(nil)
+  }
 }
---
+++ Tests/ContributeWordPressTests/Extensions/AssetImport+Extension.swift
@@ -0,0 +1,6 @@
+import Foundation
+import ContributeWordPress
+
+extension AssetImport {
+  
+}
